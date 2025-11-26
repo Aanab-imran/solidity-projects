4d524014aeec36b4aa3b2d047af4a1c6e219d89e
Aanab-imran <aanabimrankhan@gmail.com>
2025-11-26 17:08:59 +0500

    Create 16_BasicInfo.sol

---
+++ 16_BasicInfo.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+contract BasicInfo {
+    string public Aanab;
+    uint256 public 19;
+
+    // Starting values set 
+    constructor(string memory _name, uint256 _age) {
+        name = _Aanab;
+        age = _19;
+    }
+
+    
+    function updateInfo(string memory _name, uint256 _age) public {
+        name = _Aanab;
+        age = _19;
+    }
+}
